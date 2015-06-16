54d128950d1e879e485f2974c6035bd424263b36
Hansen Gustavo <gustavo.a.hansen@gmail.com>
2015-06-16 10:59:36 -0300

    Fixed unit test

--- test/core_tests/core_tests/SampleServiceTests.m
+++ test/core_tests/core_tests/SampleServiceTests.m
@@ -1,80 +1,70 @@
 
 #import <XCTest/XCTest.h>
 
-//#import <impl/impl.h>
-//#import "sample_service.h"
+#import <impl/impl.h>
+#import "sample_service.h"
 
 @interface SampleServiceTests : XCTestCase
 
-//@property MSSampleContainerClient* client;
+@property MSSampleContainerClient *client;
 @property bool running;
+
 @end
 
 @implementation SampleServiceTests
 
 - (void)setUp {
     [super setUp];
-
-  //  MSOrcDefaultDependencyResolver* resolver = [MSOrcDefaultDependencyResolver alloc];
-   // MSOrcOAuthCredentials* credentials = [MSOrcOAuthCredentials alloc];
-
-    //[credentials setToken:@"fakeToken"];
-
-    //self.client = [[MSSampleContainerClient alloc] initWithUrl:@"http://localhost:8080/" dependencyResolver:resolver];
+    
+    MSOrcDefaultDependencyResolver* resolver = [MSOrcDefaultDependencyResolver alloc];
+    MSOrcOAuthCredentials* credentials = [MSOrcOAuthCredentials alloc];
+    
+    [credentials setToken:@"fakeToken"];
+    
+    self.client = [[MSSampleContainerClient alloc] initWithUrl:@"http://localhost:8080/" dependencyResolver:resolver];
 }
 
 - (void)tearDown {
     [super tearDown];
 }
 
-
 //TODO: Enable when serialization issue with ints is ready
-/*
- -(void) testTwoParamsActionsFirstIsEntityTypeUri{
- //twoParamsActionsFirstIsEntityTypePOST.json
- self.running = true;
- __block int result = 0;
- 
- NSURLSessionDataTask* task = [[[self.client getMe] getOperations] twoParamsActionsFirstIsEntityType:[self getSampleEntity] :false :^(int returnValue, MSOrcException *error) {
- 
- result = returnValue;
- self.running = false;
- }];
- 
- [task resume];
- 
- [self blockUntilFinish:task];
- 
- XCTAssertEqual(result, 1);
- }*/
+
+- (void)testTwoParamsActionsFirstIsEntityTypeUri {
+    //twoParamsActionsFirstIsEntityTypePOST.json
+    self.running = true;
+    
+    __block int result = 0;
+    
+    [self.client.me.operations twoParamsActionsFirstIsEntityTypeWithAnEntity:[self getSampleEntity] booleanParams:false callback:^(int returnValue, MSOrcError *error) {
+        
+        result = returnValue;
+        self.running = false;
+    }];
+    
+    [self blockUntilFinish];
+    
+    XCTAssertEqual(result, 1);
+}
 
 - (void)testGetNavigationList {
     //getNavigationsGET.json
-    /*self.running = true;
+    self.running = true;
     
     __block NSArray<MSSampleContainerAnotherEntity> *entities = nil;
     
     
     [self.client.me.navigations readWithCallback:^(NSArray<MSSampleContainerAnotherEntity> *anotherEntitys, MSOrcError *error) {
+        
         entities =anotherEntitys;
-        self.running = false;
-        
-        XCTAssertEqual([entities count], 1);
-        
-    }];*/
-    /*NSURLSessionTask* task = [self.client.me.navigations readWithCallback:^(NSArray<MSSampleContainerAnotherEntity> *anotherEntities, MSOrcException *error) {
-        
-        entities =anotherEntities;
         self.running = false;
     }];
     
-    [task resume];
-    */
-    //[self blockUntilFinish:task];
+    [self blockUntilFinish];
     
-    XCTAssertEqual(1, 1);
+    XCTAssertEqual([entities count], 1);
 }
-/*
+
 - (void)testGetNavigationItem {
     //getNavigationItemGET.json
     
@@ -82,15 +72,11 @@
     
     __block MSSampleContainerAnotherEntity *entity = nil;
     
-    NSURLSessionTask* task = [[[self.client.me getNavigations] getById:@"SomeId" ] readWithCallback:^(MSSampleContainerAnotherEntity *anotherEntity, MSOrcException *error) {
+    [[self.client.me.navigations getById:@"SomeId"] readWithCallback:^(MSSampleContainerAnotherEntity *anotherEntity, MSOrcError *error) {
         
         entity =anotherEntity;
         self.running = false;
     }];
-    
-    [task resume];
-    
-    [self blockUntilFinish:task];
     
     XCTAssertNotNil(entity);
     XCTAssertTrue([entity.SomeString isEqualToString:[self getAnotherEntity].SomeString]);
@@ -103,15 +89,13 @@
     
     __block MSSampleContainerAnotherEntity *entity = nil;
     
-    NSURLSessionTask* task = [[[[[self.client getMe] getNavigations] getById:@"SomeId" ] select:@"SomeProp, AnotherProp"] readWithCallback:^(MSSampleContainerAnotherEntity *anotherEntity, MSOrcException *error) {
+    [[[self.client.me.navigations getById:@"SomeId"] select:@"SomeProp, AnotherProp"] readWithCallback:^(MSSampleContainerAnotherEntity *anotherEntity, MSOrcError *error) {
         
         entity =anotherEntity;
         self.running = false;
     }];
     
-    [task resume];
-    
-    [self blockUntilFinish:task];
+    [self blockUntilFinish];
     
     XCTAssertNotNil(entity);
     XCTAssertTrue([entity.SomeString isEqualToString:[self getAnotherEntity].SomeString]);
@@ -121,20 +105,20 @@
 
 - (void)testGetNavigationItemRawWithSelect {
     //getNavigationItemWithSelectGET.json
+    
     self.running = true;
     __block NSString *jsonResult = nil;
     
-    NSURLSessionTask* task = [[[[[self.client getMe]  getNavigations] getById:@"SomeId" ]  select:@"SomeProp, AnotherProp" ] readRawWithCallback:^(NSString *result, MSOrcException *error) {
+    [[[self.client.me.navigations getById:@"SomeId"]  select:@"SomeProp, AnotherProp" ] readRawWithCallback:^(NSString *result, MSOrcError *error) {
         
         jsonResult =result;
         self.running = false;
     }];
     
-    [task resume];
-    
-    [self blockUntilFinish:task];
+    [self blockUntilFinish];
     
     NSString *expectedResponseString = @"{\"SomeString\":\"Some String\",\"Id\":\"3281EC0B-1AEB-49A4-A345-E64D732DA6D3\",\"@odata.type\":\"#Microsoft.SampleService.AnotherEntity\"}";
+    
     XCTAssertNotNil(jsonResult);
     XCTAssertTrue([jsonResult isEqualToString:expectedResponseString]);
 }
@@ -146,15 +130,13 @@
     
     __block NSArray<MSSampleContainerAnotherEntity> *entities = nil;
     
-    NSURLSessionTask* task = [[[[self.client.me getNavigations] select:@"SomeProp, AnotherProp" ] top:1] readWithCallback:^(NSArray<MSSampleContainerAnotherEntity> *anotherEntities, MSODataException *error) {
+    [[[self.client.me.navigations select:@"SomeProp, AnotherProp"] top:1] readWithCallback:^(NSArray<MSSampleContainerAnotherEntity> *anotherEntities, MSOrcError *error) {
         
         entities =anotherEntities;
         self.running = false;
     }];
     
-    [task resume];
-    
-    [self blockUntilFinish:task];
+    [self blockUntilFinish];
     
     XCTAssertEqual([entities count], 1);
 }
@@ -169,15 +151,13 @@
     
     __block NSArray<MSSampleContainerSampleEntity> *entities = nil;
     
-    NSURLSessionTask* task = [[[self.client.services expand:@"SomeProp"] filter:@"SomeProp eq 'SomeString'" ] readWithCallback:^(NSArray<MSSampleContainerSampleEntity> *sampleEntitys, MSODataException *error) {
+    [[[self.client.services expand:@"SomeProp"] filter:@"SomeProp eq 'SomeString'"] readWithCallback:^(NSArray<MSSampleContainerSampleEntity> *sampleEntitys, MSOrcError *error) {
         
         entities =sampleEntitys;
         self.running = false;
     }];
     
-    [task resume];
-    
-    [self blockUntilFinish:task];
+    [self blockUntilFinish];
     
     XCTAssertEqual([entities count], 1);
 }
@@ -189,15 +169,15 @@
     
     __block NSArray<MSSampleContainerSampleEntity> *entities = nil;
     
-    NSURLSessionTask* task = [[[[self.client getservices] addCustomHeaderWithName:@"Header1" value:@"Value1"] addCustomHeaderWithName:@"Header2" value:@"Value2"] readWithCallback:^(NSArray<MSSampleContainerSampleEntity> *sampleEntitys, MSODataException *error) {
+    [[[self.client.services addCustomHeaderWithName:@"Header1" value:@"Value1"]
+                            addCustomHeaderWithName:@"Header2" value:@"Value2"]
+                                   readWithCallback:^(NSArray<MSSampleContainerSampleEntity> *sampleEntitys, MSOrcError *error) {
         
         entities =sampleEntitys;
         self.running = false;
     }];
     
-    [task resume];
-    
-    [self blockUntilFinish:task];
+    [self blockUntilFinish];
     
     XCTAssertEqual([entities count], 1);
 }
@@ -209,15 +189,13 @@
     self.running = true;
     __block NSArray<MSSampleContainerSampleEntity> *entities = nil;
     
-    NSURLSessionTask* task = [[[self.client getservices] top:99 ] readWithCallback:^(NSArray<MSSampleContainerSampleEntity> *sampleEntitys, MSODataException *error) {
+    [[self.client.services top:99] readWithCallback:^(NSArray<MSSampleContainerSampleEntity> *sampleEntitys, MSOrcError *error) {
         
         entities =sampleEntitys;
         self.running = false;
     }];
     
-    [task resume];
-    
-    [self blockUntilFinish:task];
+    [self blockUntilFinish];
     
     XCTAssertEqual([entities count], 1);
 }
@@ -227,17 +205,15 @@
     
     self.running = true;
     
-    __block MSODataException *expectedError = nil;
+    __block MSOrcError *expectedError = nil;
     
-    NSURLSessionTask* task = [[[[[self.client getMe] getNavigations] getById:@"SomeId"] addCustomHeaderWithName:@"If-Match" value:@"*"] deleteWithCallback:^(int statusCode, MSODataException *exception) {
+    [[[self.client.me.navigations getById:@"SomeId"] addCustomHeaderWithName:@"If-Match" value:@"*"] deleteWithCallback:^(int statusCode, MSOrcError *error) {
         
-        expectedError = exception;
+        expectedError = error;
         self.running = false;
     }];
     
-    [task resume];
-    
-    [self blockUntilFinish:task];
+    [self blockUntilFinish];
     
     XCTAssertNil(expectedError);
 }
@@ -247,15 +223,13 @@
     self.running = true;
     __block MSSampleContainerAnotherEntity *entity = nil;
     
-    NSURLSessionTask* task = [[self.client.me getNavigations] addAnotherEntity:[self getAnotherEntity] callback:^(MSSampleContainerAnotherEntity *anotherEntity, MSODataException *e) {
+    [self.client.me.navigations add:[self getAnotherEntity] callback:^(MSSampleContainerAnotherEntity *anotherEntity, MSOrcError *e) {
         
         entity =anotherEntity;
         self.running = false;
     }];
     
-    [task resume];
-    
-    [self blockUntilFinish:task];
+    [self blockUntilFinish];
     
     XCTAssertNotNil(entity);
     XCTAssertTrue([entity.SomeString isEqualToString:[self getAnotherEntity].SomeString]);
@@ -269,15 +243,15 @@
     MSSampleContainerAnotherEntity *entityToUpdate = [self getAnotherEntity];
     entityToUpdate.SomeString = @"Some Updated String";
     
-    NSURLSessionTask* task = [[[[_client.me getNavigations] getById:entityToUpdate.Id] addCustomHeaderWithName:@"IsPatch" value:@"yes"] updateEntity:entityToUpdate callback:^(id updatedEntity, MSODataException *exception) {
+    [[[_client.me.navigations getById:entityToUpdate.Id]
+      addCustomHeaderWithName:@"IsPatch" value:@"yes"]
+                       update:entityToUpdate callback:^(id updatedEntity, MSOrcError *error) {
         
         entity =updatedEntity;
         self.running = false;
     }];
     
-    [task resume];
-    
-    [self blockUntilFinish:task];
+    [self blockUntilFinish];
     
     XCTAssertNotNil(entity);
     XCTAssertTrue([entity.SomeString isEqualToString:entityToUpdate.SomeString]);
@@ -288,22 +262,20 @@
     self.running = true;
     __block NSArray<MSSampleContainerAnotherEntity> *entities = nil;
     
-    NSURLSessionTask* task = [[[[self.client getMe] getNavigations] addCustomParametersWithName:@"StringParam" value:@"SomeValue"] readWithCallback:^(NSArray<MSSampleContainerAnotherEntity> *anotherEntitys, MSOrcException *error) {
+    [[self.client.me.navigations addCustomParametersWithName:@"StringParam" value:@"SomeValue"] readWithCallback:^(NSArray<MSSampleContainerAnotherEntity> *anotherEntitys, MSOrcError *error) {
         
         entities =anotherEntitys;
         self.running = false;
     }];
     
-    //.addParameter("StringParam", param1)
-    [task resume];
-    
-    [self blockUntilFinish:task];
+    [self blockUntilFinish];
     
     XCTAssertNotNil(entities);
     XCTAssertEqual([entities count], 1);
     
 }
 
+/*
 -(void) testEnumParser{
     
     self.running = true;
@@ -315,10 +287,10 @@
     id entity = [[[self.client resolver] jsonSerializer] deserialize:data asClass:[t class]];
     
     XCTAssertNotNil(entity);
-}
-*/
+}*/
+
 //TODO: Reactor this: add semaphore or get method to terminator methods
-/*-(void)blockUntilFinish : (NSURLSessionTask*)task{
+-(void)blockUntilFinish {
     
     while (self.running){}
 }
@@ -336,6 +308,6 @@
     anotherEntity.Id = @"3281EC0B-1AEB-49A4-A345-E64D732DA6D3";
     anotherEntity.SomeString=@"Some String";
     return anotherEntity;
-}*/
+}
 
 @end
